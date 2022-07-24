df1bb2cce10ae597d0d0490b615522ede9295edf
Mukund Madhav <mukundmadhav777@gmail.com>
2022-07-24 22:27:50 +0530

    first commit

--- NotesSharingProject/settings.py
+++ NotesSharingProject/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+# import django_heroku
+# import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+import django_heroku
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -25,8 +26,8 @@ SECRET_KEY = 'django-insecure-n3w0&qhhe(wnk_a7fb*n$#8^u)*3y*u&o2k#m(2_!n-1ogq^zc
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 # DEBUG = True
-ALLOWED_HOSTS = ['127.0.0.1','online-notes-sharing.herokuapp.com']
-
+# ALLOWED_HOSTS = ['127.0.0.1','online-notes-sharing.herokuapp.com']
+ALLOWED_HOSTS = ['notessharingproject.herokuapp.com']
 
 # Application definition
 
@@ -137,3 +138,5 @@ STATIC_ROOT=os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
