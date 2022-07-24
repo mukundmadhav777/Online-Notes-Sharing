a0ef84ee218c5e3c3b562b585ccfc617da34b21b
Mukund Madhav <mukundmadhav777@gmail.com>
2022-07-24 22:41:10 +0530

    first commit

--- NotesSharingProject/settings.py
+++ NotesSharingProject/settings.py
@@ -134,7 +134,7 @@ STATIC_URL = '/static/'
 STATIC_URL='/media/'
 STATIC_ROOT=os.path.join(BASE_DIR,'media')
 
-# Default primary key field type
+
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
